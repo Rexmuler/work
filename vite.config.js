593c327d1b735c308e2e21a0ca73288879410f4d
unknown <rexmuler@gmail.com>
2024-11-27 09:09:11 -0900

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  base:"/vite-login/",
-=======
-  base:"/Login/",
-
->>>>>>> 99876dbee04fe09dcf738501c0323c1d3b746897
-})
+});
